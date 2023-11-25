9b86057d28c69fad071bb1c727ccbe991256ecc5
unknown <kaustubhpatil418@gmail.com>
2023-11-25 20:13:53 +0530

    NewCommit

--- src/Whatsapp/whatsapp.js
+++ src/Whatsapp/whatsapp.js
@@ -4,16 +4,11 @@ import QRCode from "react-qr-code";
 import axios from "axios";
 import { common_Link } from "../commonlink";
 import { ToastContainer, toast } from "react-toastify";
-import { ClimbingBoxLoader } from "react-spinners";
-import { commonLink2 } from "../commonLink2";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { BeatLoader } from "react-spinners";
 
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
-
+import {Qrcode } from 'react-qr-code'
 export default function Whatsapp() {
   const currentTime = new Date(); // Create a new Date object with the current date and time
   
@@ -21,8 +16,8 @@ export default function Whatsapp() {
   var [loading, setloading] = useState(true);
   var [phone, setphone] = useState("");
   var [message, setmessage] = useState("");
-
-  useEffect(() => {});
+  
+  useEffect(() => {},[value,loading]);
   return (
     <>
       <MiniDrawer />
@@ -55,7 +50,13 @@ export default function Whatsapp() {
           onClick={async () => {
             toast.warning("It Will take 40sec to 1min to assign please Wait");
             await axios
-              .post(common_Link+'/task/whatsapp', {
+              .post('http://localhost:5000/task/whatsapp',)
+              .then((resp) => {
+               
+               setvalue((resp.data.qr))
+               setloading((false))
+               
+               axios.post('http://localhost:5000/task/whatsappsave',{
                 message: message,
                 phone_number: phone,
                 hours: currentTime.getHours(),
@@ -63,14 +64,14 @@ export default function Whatsapp() {
                 assignedTo: phone,
                 projectname: localStorage.getItem("projectname"),
                email: localStorage.getItem("email"),
-              })
-              .then((resp) => {
-                if (resp.status == 200) {
-                  toast.success("Message Sent Check to Whatsapp  Task");
-                 
-                } else {
-                  toast.error("Something Went Wrong");
-                }
+               }).then(resp=>{
+
+                toast.success('Message Sent Succesfully')
+                setloading((true))
+               }).catch(err=>{
+                toast.error("Something Went Wrong")
+               })
+
               })
               .catch((err) => {
                 console.log(err);
@@ -80,6 +81,12 @@ export default function Whatsapp() {
         >
           Create Task{" "}
         </button>
+
+      <h3>Scan below QRCode</h3>
+      {loading?<BeatLoader size={50} color="grey" />:<QRCode  size={256}
+    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+    value={value}
+    viewBox={`0 0 256 256`}/>}
       </div>
     </>
   );
